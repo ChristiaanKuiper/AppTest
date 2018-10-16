064cd652ca5037e95d9c055dee4ecf1d76c046e5
Christiaan Kuiper <ckuiper@vxfactory.nl>
2018-10-16 15:45:25 +0200

    Initial Commit

---
+++ AppTest.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using AppTest.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppTest.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ AppTest.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace AppTest.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set;}
+        public string Name { get; set; }
+    }
+}
